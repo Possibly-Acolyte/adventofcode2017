acc04ef9e45075aba742d9bc0ea1da936b984764
Edward Demey <edward.demey@gmail.com>
2017-12-02 15:48:29 +0100

    Cleanup
    Removed unnecessary print + cleanup code

--- AoC_1.ps1
+++ AoC_1.ps1
@@ -1,18 +1,14 @@
-$CAPTCHA = read-host "De input om te solven"
+$CAPTCHA = read-host "1. GIVE THE INPUT STRING"
 $CAPTCH_ARRAY = $CAPTCHA.ToCharArray()
 $SUM = 0
-for($i=0;$i -lt $CAPTCH_ARRAY.length -1; $i++){
-	if($CAPTCH_ARRAY[$i] -eq $CAPTCH_ARRAY[$i+1]){
+for($i=0;$i -lt $CAPTCH_ARRAY.length; $i++){
+	if($CAPTCH_ARRAY[$i] -eq $CAPTCH_ARRAY[($i+1)%$CAPTCH_ARRAY.length]){
 		$SUM += ([convert]::ToInt32($CAPTCH_ARRAY[$i], 10))
 	}
 }
-write-host "before end" $SUM
-if($CAPTCH_ARRAY[$i] -eq $CAPTCH_ARRAY[0]){
-		$SUM += ([convert]::ToInt32($CAPTCH_ARRAY[$i], 10))
-}
-write-host "DE SUM IS $SUM"
+write-host "THE SUM IS $SUM"
 
-$CAPTCHA = read-host "De input om te solven 2"
+$CAPTCHA = read-host "2. GIVE THE INPUT STRING"
 $CAPTCH_ARRAY = $CAPTCHA.ToCharArray()
 $SUM = 0
 $OFFSETT =$CAPTCH_ARRAY.length/2
@@ -21,5 +17,5 @@ for($i=0;$i -lt $CAPTCH_ARRAY.length; $i++){
 		$SUM += ([convert]::ToInt32($CAPTCH_ARRAY[$i], 10))
 	}
 }
-write-host "DE SUM IS $SUM"
+write-host "THE SUM IS $SUM"
 read-host
