73c58dd89a6af6e05f6a4f66aa42b5155362116a
Edward Demey <edward.demey@gmail.com>
2017-12-03 17:54:51 +0100

    solution part2
    added the solution for part2, can be cleaned up a lot
    added a small line in the main file as well to run the code, but its irrelevant enough not to upload the change

--- day3/day3.java
+++ day3/day3.java
@@ -5,6 +5,8 @@
  */
 package adventofcode;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Edward
@@ -43,4 +45,127 @@ public class day3 {
         }
         return ((int)Math.abs(p[0]-q[0])) + ((int)Math.abs(p[1]-q[1]));
     }
+   
+    public int part2(int input){   //brute force
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        String direction = "right";
+        int[] coordinates = new int[]{0,0};
+        ArrayList<Integer> templist = new ArrayList<>();
+        templist.add(1);
+        list.add(templist);
+        while(true){
+            switch(direction){
+                case "right":
+                    if(coordinates[1] == list.get(coordinates[0]).size()-1){    //on the edge of the memory
+                        expand(list);
+                        coordinates[0]++; coordinates[1]++;
+                    }
+                    coordinates[1]++;
+                    list.get(coordinates[0]).set(coordinates[1], calculateValue(list, coordinates));
+                    if(list.get(coordinates[0]).get(coordinates[1]) > input){
+                        for(ArrayList<Integer> print : list){
+                            for(int nr : print){
+                                System.out.print(" " + nr + " ");
+                            } 
+                            System.out.println("");
+                        }
+                        return list.get(coordinates[0]).get(coordinates[1]);
+                    }
+                    if(list.get(coordinates[0]-1).get(coordinates[1]) == 0){
+                        direction = "up";
+                    }
+                    break;
+                case "left":
+                    if(coordinates[1] == 0){    //on the edge of the memory
+                        expand(list);
+                        coordinates[0]++; coordinates[1]++;
+                    }
+                    coordinates[1]--;
+                    list.get(coordinates[0]).set(coordinates[1], calculateValue(list, coordinates));
+                    if(list.get(coordinates[0]).get(coordinates[1]) > input){
+                        for(ArrayList<Integer> print : list){
+                            for(int nr : print){
+                                System.out.print(" " + nr + " ");
+                            } 
+                            System.out.println("");
+                        }
+                        return list.get(coordinates[0]).get(coordinates[1]);
+                    }                    
+                    if(list.get(coordinates[0]+1).get(coordinates[1]) == 0){
+                        direction = "down";
+                    }
+                    break;
+                case "up":
+                    if(coordinates[0] == 0){    //on the edge of the memory
+                        expand(list);
+                        coordinates[0]++; coordinates[1]++;
+                    }
+                    coordinates[0]--;
+                    list.get(coordinates[0]).set(coordinates[1], calculateValue(list, coordinates));
+                    if(list.get(coordinates[0]).get(coordinates[1]) > input){
+                        for(ArrayList<Integer> print : list){
+                            for(int nr : print){
+                                System.out.print(" " + nr + " ");
+                            } 
+                            System.out.println("");
+                        }
+                        return list.get(coordinates[0]).get(coordinates[1]);
+                    }                    
+                    if(list.get(coordinates[0]).get(coordinates[1]-1) == 0){
+                        direction = "left";
+                    }
+                    break;
+                case "down":
+                    if(coordinates[0] == list.size()-1){    //on the edge of the memory
+                        expand(list);
+                        coordinates[0]++; coordinates[1]++;
+                    }
+                    coordinates[0]++;
+                    list.get(coordinates[0]).set(coordinates[1], calculateValue(list, coordinates));
+                    if(list.get(coordinates[0]).get(coordinates[1]) > input){
+                        for(ArrayList<Integer> print : list){
+                            for(int nr : print){
+                                System.out.print(" " + nr + " ");
+                            } 
+                            System.out.println("");
+                        }
+                        return list.get(coordinates[0]).get(coordinates[1]);
+                    }
+                    
+                    if(list.get(coordinates[0]).get(coordinates[1]+1) == 0){
+                        direction = "right";
+                    }
+                    break;
+            }
+        }
+    }
+    
+    private int calculateValue(ArrayList<ArrayList<Integer>> list, int[]coordinates){
+        int sum = 0;
+        for(int i = -1; i<2; i++){
+            for(int j = -1; j<2; j++){
+                if(!(i == 0 && j == 0) && coordinates[0] + i >= 0 && coordinates[1] + j >= 0 && list.size() > coordinates[0] + i && list.get(coordinates[0] + i).size()>coordinates[1] + j){
+                    sum+= list.get(coordinates[0] + i).get(coordinates[1] + j);
+                }
+            }
+        }
+        return sum;
+    }
+    
+    private void expand(ArrayList<ArrayList<Integer>> lists){   //expand the memory by 1 on each side
+        for(ArrayList<Integer> list : lists){
+            list.add(0, 0);     //add 2 empty elements in the memory
+            list.add(0);
+        }
+        ArrayList<Integer> templist = new ArrayList<>();
+        ArrayList<Integer> templist2 = new ArrayList<>();
+        for(int i: lists.get(0)){
+            templist.add(0);
+            templist2.add(0);
+        }
+        lists.add(0, templist);
+        lists.add(templist2);
+        
+    }
 }
+
